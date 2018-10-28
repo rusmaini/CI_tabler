3664ed24c8b9f2e2a72c8280550d2b15b8b05db0
rusmaini <rusmaini@gmail.com>
2018-10-28 17:14:33 +0800

    -

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,33 +6,33 @@
               </div>
               <?=form_open('auth/login',array('class'=>'card')); ?>
                 <div class="card-body p-6">
-                  <div class="card-title">Login to your account</div>
+                  <div class="card-title">Log masuk ke akaun anda</div>
                   <div class="form-group">
-                    <label class="form-label">Email address</label>
-                    <input type="email" name="email" value="<?=set_value('email')?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <label class="form-label">Emel</label>
+                    <input type="email" name="email" value="<?=set_value('email')?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan emel">
                     <?= form_error('email'); ?>
                   </div>
                   <div class="form-group">
                     <label class="form-label">
-                      Password
-                      <a href="<?=base_url()?>auth/lupa" class="float-right small">I forgot password</a>
+                      Kata laluan
+                      <a href="<?=base_url()?>auth/lupa" class="float-right small">Saya lupa kata laluan</a>
                     </label>
-                    <input type="password" name="password" value="<?=set_value('password')?>" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" name="password" value="<?=set_value('password')?>" class="form-control" id="exampleInputPassword1" placeholder="Kata laluan">
                     <?= form_error('password'); ?>
                   </div>
                   <div class="form-group">
                     <label class="custom-control custom-checkbox">
                       <input type="checkbox" class="custom-control-input" />
-                      <span class="custom-control-label">Remember me</span>
+                      <span class="custom-control-label">Ingat saya</span>
                     </label>
                   </div>
                   <div class="form-footer">
-                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                    <button type="submit" class="btn btn-primary btn-block">Log masuk</button>
                   </div>
                 </div>
               <?=form_close(); ?>
               <div class="text-center text-muted">
-                Don't have account yet? <a href="<?=base_url()?>register">Sign up</a>
+                Belum ada akaun? <a href="<?=base_url()?>register">Daftar akaun</a>
               </div>
             </div>
           </div>
